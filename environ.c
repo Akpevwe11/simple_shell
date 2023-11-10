5d400a5bed7d7e5483d08b2b4127996faea6ea25
praisex64 <essiakpevwepraise@gmail.com>
2023-11-10 07:42:05 -0800

    modify the documentation for the environment variables

--- environ.c
+++ environ.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _myenv - prints the current environment variables to the console
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
@@ -13,7 +13,7 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets the value of an environ variable
+ * _getenv - gets the value of an environ variable of the current environment
  * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
  *
