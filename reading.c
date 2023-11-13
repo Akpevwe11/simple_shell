7fbe436e55c21377321b337f48c6e2603ef7f55f
praisex64 <essiakpevwepraise@gmail.com>
2023-11-13 08:17:15 -0800

    feature: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

--- reading.c
+++ reading.c
@@ -21,9 +21,8 @@ char *reading(void)
 	size_t buff_size = 0;
 	ssize_t bytes_read = 0; 
 
-	container = getline(&line, &buff_size, stdin);
+	bytes_read = getline(&line, &buff_size, stdin);
 	if(bytes_read == -1)
-		free(line);
-		exit(0);
+		free(line),exit(0);
 	return (line);
 }
